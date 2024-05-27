dc11ea68452b11a63337cad7e5e42d080bae457b
Eunice Dickson <sunshinebae439@gmail.com>
2024-05-27 07:15:14 +0000

    Made my name to be displayed as bold

--- App.js
+++ App.js
@@ -2,22 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const myName = 'Eunice'; 
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    texts: {
+      fontSize: '24',
+    },
+    textStyle: {
+      fontWeight: `${myName} === 'Eunice' ? 'bold' : 'normal'`,
+    }
+  });
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.texts}>My name is Eunice</Text>
+      <Text style={styles.texts}>My name is {myName}</Text>
       <StatusBar style={styles.auto} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  texts: {
-    fontSize: '24'
-  }
-});
+
