f82fe3b781493658c7c19a1e534423ddd2c5c469
Eunice Dickson <sunshinebae439@gmail.com>
2024-05-27 07:29:02 +0000

    Created a react-native app using the Expo CLI

--- App.js
+++ App.js
@@ -2,29 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const myName = 'Eunice'; 
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    texts: {
-      fontSize: '24',
-    },
-    textStyle: {
-      fontWeight: `${myName} === 'Eunice' ? 'bold' : 'normal'`,
-    }
-  });
-  
   return (
     <View style={styles.container}>
-      <Text style={styles.texts}>My name is {myName}</Text>
-      <StatusBar style={styles.auto} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
